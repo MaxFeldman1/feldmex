c9890ec4389f90846e392a5d8594de944b472955
MaxFeldman1 <max.d.feldman@protonmail.com>
2019-12-18 22:04:36 -0600

    gave oracle functionality for storeing and fetching previous spot prices

--- contracts/oracle.sol
+++ contracts/oracle.sol
@@ -3,15 +3,29 @@ pragma solidity ^0.5.12;
 contract oracle{
     uint btceth;
     
+    //height => price
+    mapping(uint => uint) public spots;
+
     function set(uint _btceth) public {
         btceth = _btceth;
+        spots[block.number] = btceth;
     }
     
     function get() public view returns(uint) {
         return btceth;
     }
     
+    function getUint(uint height) public view returns(uint){
+        while (height > 1){
+            if (spots[height] > 0)
+                return spots[height];
+            height--;
+        }
+        return 0;
+    }
+
     function height() public view returns(uint){
         return block.number;
     }
+
 }
--- test/oracle.js
+++ test/oracle.js
@@ -2,14 +2,35 @@ var oracle = artifacts.require("./oracle.sol");
 
 contract('oracle', function(accounts){
 
-	it('val == set spot', function(){
+	it('sets and fetches spot price', function(){
 		return oracle.deployed().then((instance) => {
 			orcInstance = instance;
-			return orcInstance.set(5);
+			spot = 5
+			secondSpot = 7;
+			return orcInstance.set(spot);
 		}).then(() => {
 			return orcInstance.get();
-		}).then((val) => {
-			assert.equal(val, 5, 'val == set spot');
+		}).then((res) => {
+			assert.equal(res.toNumber(), spot, 'get(void) fetches current spot price');
+			return orcInstance.height();
+		}).then((res) => {
+			height = res.toNumber();
+			return orcInstance.getUint(height);
+		}).then((res) => {
+			assert.equal(res.toNumber(), spot, "getUint(uint) fetches the latest spot price");
+			return orcInstance.set(secondSpot);
+		}).then(() => {
+			//note that we have not updated the value of height yet
+			return orcInstance.getUint(height);
+		}).then((res) => {
+			assert.equal(res.toNumber(), spot, "getUint(uint) can fetch previous values");
+			//we are now feching the price of the blocks after setting the spot a second time
+			return orcInstance.getUint(height+2);
+		}).then((res) => {
+			assert.equal(res.toNumber(), secondSpot, "getUint(uint) can fetch the most recent spot");
+			return orcInstance.getUint(height-1);
+		}).then((res) => {
+			assert.equal(res.toNumber(), 0, "getUint(uint) returns 0 when there are no previous spot prices");
 		});
 	});
 });
