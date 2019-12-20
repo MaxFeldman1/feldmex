3f67a5da1583d450f7b6cff01571ac6c3017aad4
MaxFeldman1 <max.d.feldman@protonmail.com>
2019-12-19 23:35:16 -0600

    reworked scripts/process/start.js to be compatible with the new calls.sol functionality

--- scripts/process/start.js
+++ scripts/process/start.js
@@ -5,6 +5,15 @@ module.exports = function(callback){
 	calls = artifacts.require("./calls.sol");
 	collateral = artifacts.require("./collateral.sol");
 
+	originalSpot = 100;
+	finalSpot = 150;
+	strike = originalSpot;
+	price = 177777;
+	buyAmount = 100;
+	sellAmount = 55;
+
+	const defaultBytes32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
 	//set instances for each artifact
 	oracle.deployed().then((i) => {
 		oracleInstance = i;
@@ -25,7 +34,6 @@ module.exports = function(callback){
 		return tokenInstance.satUnits();
 	}).then((res) => {
 		satUnits = res.toNumber();
-		originalSpot = 100;
 		oracleInstance.set(originalSpot);
 	}).then((res) => {
 		transferAmount = 1000;
@@ -37,10 +45,8 @@ module.exports = function(callback){
 		console.log("Total reciver balance: " +uint.toNumber());
 		return tokenInstance.approve(collateral.address, transferAmount, true); // from defaultAccount
 	}).then((res) => {
-		console.log(res.logs[0].event + ' ' + res.logs[0].args._value.toNumber()/satUnits);
 		return tokenInstance.approve(collateral.address, transferAmount, true, {from: reciverAccount});
 	}).then((res) => {
-		console.log(res.logs[0].event + ' ' + res.logs[0].args._value.toNumber()/satUnits);
 		return collateralInstance.postCollateral(transferAmount, true);
 	}).then((res) => {
 		return collateralInstance.postCollateral(transferAmount, true, {from: reciverAccount});
@@ -56,52 +62,48 @@ module.exports = function(callback){
 	}).then((res) => {
 		blockHeight = res.toNumber();
 		console.log("Block Height: " + blockHeight);
-		maturity = blockHeight+10;
+		maturity = blockHeight+2;
 		console.log(maturity+' ' +originalSpot);
-		return collateralInstance.postBuy(maturity, originalSpot, 177777, 100, {from: defaultAccount});
+		return collateralInstance.postBuy(maturity, strike, price, buyAmount, {from: defaultAccount});
 	}).then((res) => {
 		console.log("Buy Posted, now reciver is making marketSell");
-		return collateralInstance.marketSell(maturity, originalSpot, 55, {from: reciverAccount});
+		return collateralInstance.marketSell(maturity, strike, sellAmount, {from: reciverAccount});
 	}).then((res) => {
 		console.log("Market Sell complete");
-		return oracleInstance.set(originalSpot + 50);
+		return oracleInstance.set(finalSpot);
 	}).then((res) => {
-		return callsInstance.myContracts({from: defaultAccount});
+		//return callsInstance.myContracts({from: defaultAccount});
+		return callsInstance.claim(maturity, strike, {from: defaultAccount});
 	}).then((res) => {
-		contracts = res;
-		console.log("Exercizing/recliaming all contracts");
-		console.log("# of contracts: " + contracts.length);
-		contracts.forEach((res0) => {
-			callsInstance.allCalls(res0).then((res1) => {
-				console.log("Maturity: " + res1.maturity.toNumber());
-				web3.eth.getBlockNumber().then((res) => {
-					blockHeight = res;
-					console.log("Height: " + blockHeight);
-					if (res1.maturity.toNumber() < blockHeight){
-						callsInstance.reclaim(res0, {from: res1.debtor}).then(() => console.log("Reclaimed")).catch((err) => {console.log("Reclaim Error")});
-					}
-					else {
-						callsInstance.exercize(res0, {from: res1.holder}).then(() => console.log("Exercized")).catch((err) => {console.log("Exercize Error")});
-					}
-				});
+		return callsInstance.claim(maturity, strike, {from: reciverAccount});
+	}).then(() => {
+		return callsInstance.withdrawFunds({from: defaultAccount});
+	}).then(() => {
+		return callsInstance.withdrawFunds({from: reciverAccount});
+	}).then(async () => {
+		for (var cont = true; cont;){
+			await collateralInstance.listHeads(maturity, strike, 0).then((res) => {
+				if (res == defaultBytes32) {cont = false; return;}
+				return collateralInstance.linkedNodes(res).then((res1) => {
+					return collateralInstance.offers(res1.hash).then((res2) => {
+						return collateralInstance.cancelBuy(res1.name, {from: res2.offerer});
+					})
+				})
 			});
-		});
-		return;
+		}
 	}).then((res) => {
 		return collateralInstance.withdrawMaxCollateral({from: defaultAccount});
 	}).then((res) => {
-		console.log("Identifier numbero En");
 		return collateralInstance.withdrawMaxCollateral({from: reciverAccount});
 	}).then((res) => {
-		console.log("Identifier numbero to");
 		return tokenInstance.addrBalance(defaultAccount, false);
 	}).then((res) => {
-		console.log("Default Address Balance: " + res/satUnits);
+		console.log("Default Address Balance: " + res.toNumber()/satUnits);
 		return tokenInstance.addrBalance(reciverAccount, false);
 	}).then((res) => {
-		console.log("Reciver Account Balance " + res/satUnits);
-		return collateralInstance.testing();
+		console.log("Reciver Account Balance " + res.toNumber()/satUnits);
+		return tokenInstance.addrBalance(collateral.address, false);
 	}).then((res) => {
-		console.log("Testing: "+res);
+		console.log("collateral contract balance: " + res.toNumber()/satUnits);
 	});
 };
